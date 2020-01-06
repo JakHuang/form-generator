77e2401883d9ff02b33a77a4bffd09916a013ac1
JakHuang <352407406@qq.com>
2020-01-06 20:34:52 +0800

    控制预览时弹框的宽度

--- src/views/preview/main.js
+++ src/views/preview/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 let $previewApp = document.getElementById('previewApp'),
 childAttrs = {
   file: '',
-  dialog: ` v-if="visible" :visible.sync="visible" :modal-append-to-body="false" `
-}
+  dialog: ` class="dialog-width" v-if="visible" :visible.sync="visible" :modal-append-to-body="false" `
+},
+dialogWidth = `<style>.dialog-width .el-dialog{ width: 90%; max-width: 666px; }</style>`
 
 window.addEventListener('message', init, false)
 
@@ -13,7 +14,7 @@ function init(event) {
       main = eval("(" + code.js + ")"),
       attrs = childAttrs[code.generateConf.type]
 
-    $previewApp.innerHTML = `<style>${code.css}</style><div id="app"></div>`
+    $previewApp.innerHTML = `<style>${code.css}</style>${dialogWidth}<div id="app"></div>`
     main.template = `<div>${code.html}</div>`
     new Vue({
       template: `<div id="${+new Date()}"><child ${attrs}/></div>`,
