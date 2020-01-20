4e8b852c2e3c4e007e2b471656cf87aaf0efa87b
IWANABETHATGUY <974153916@qq.com>
2020-01-20 17:10:02 +0800

    fix: 修复marks 参数

--- src/views/index/RightPanel.vue
+++ src/views/index/RightPanel.vue
@@ -68,7 +68,7 @@
               v-model="activeData.span"
               :max="24"
               :min="1"
-              :marks="{ 12: '12' }"
+              :marks="{ 12: '' }"
               @change="spanChange"
             />
           </el-form-item>
