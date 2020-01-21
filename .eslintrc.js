8bca09aa15857e5e77745a403eaba0d750f466ee
IWANABETHATGUY <974153916@qq.com>
2020-01-21 17:51:14 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,38 +3,38 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/recommended", "eslint:recommended", "airbnb-base"],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', 'airbnb-base'],
   rules: {
-    "linebreak-style": 0,
-    "no-console": "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/no-unused-components": 0,
-    "no-unused-vars": 0,
-    "import/extensions": 0,
-    "import/no-unresolved": 0,
-    "comma-dangle": [2, "never"],
-    semi: [2, "never"],
-    "no-unused-expressions": 0,
-    "no-plusplus": 0,
-    "import/prefer-default-export": 0,
-    "no-use-before-define": 0,
-    "no-param-reassign": 0,
-    "arrow-parens": [2, "as-needed"],
-    "vue/max-attributes-per-line": 0,
-    "max-len": [
+    'linebreak-style': 0,
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-unused-components': 0,
+    'no-unused-vars': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'comma-dangle': [2, 'never'],
+    semi: [2, 'never'],
+    'no-unused-expressions': 0,
+    'no-plusplus': 0,
+    'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+    'arrow-parens': [2, 'as-needed'],
+    'vue/max-attributes-per-line': 0,
+    'max-len': [
       1,
       {
         code: 120
       }
     ],
-    "no-eval": 0,
-    "no-multi-assign": 0,
-    "prefer-rest-params": 0,
-    "vue/require-prop-types": 0,
-    "no-restricted-globals": 0
+    'no-eval': 0,
+    'no-multi-assign': 0,
+    'prefer-rest-params': 0,
+    'vue/require-prop-types': 0,
+    'no-restricted-globals': 0
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   globals: {
     location: false
