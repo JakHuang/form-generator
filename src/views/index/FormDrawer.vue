bcbf6ee298270ea2a4596cc174a1a96b6c5d3731
IWANABETHATGUY <974153916@qq.com>
2020-01-21 17:52:43 +0800

    feat: beforeDestroy

--- src/views/index/FormDrawer.vue
+++ src/views/index/FormDrawer.vue
@@ -208,6 +208,9 @@ export default {
       }
     })
   },
+  beforeDestroy() {
+    window.removeEventListener('keydown', this.preventDefaultSave)
+  },
   methods: {
     preventDefaultSave(e) {
       if (e.key === 's' && (e.metaKey || e.ctrlKey)) {
