2bcf1f9bae0de2f90d3363ec54f56762d0674198
hj <352407406@qq.com>
2020-02-27 12:23:38 +0800

    fix: firefox 下 拖拽 会新打卡一个选项卡

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -10,6 +10,13 @@ export default {
     // 取消开始的loading动画
     const preLoader = document.querySelector('#pre-loader')
     preLoader.style.display = 'none'
+
+    // fix: firefox 下 拖拽 会新打卡一个选项卡
+    // https://github.com/JakHuang/form-generator/issues/15
+    document.body.ondrop = event => {
+      event.preventDefault()
+      event.stopPropagation()
+    }
   }
 }
 </script>
