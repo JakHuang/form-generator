7b1925a3b53ebb2b5a62ad39aa25a13ff82dff49
JakHuang <352407406@qq.com>
2020-02-28 12:54:04 +0800

    feat: 加入缓存版本的概念，保证缓存数据与程序匹配

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,19 @@
 const DRAWING_ITEMS = 'drawingItems'
+const DRAWING_ITEMS_VERSION = '1.0'
+const DRAWING_ITEMS_VERSION_KEY = 'DRAWING_ITEMS_VERSION'
 const DRAWING_ID = 'idGlobal'
 const TREE_NODE_ID = 'treeNodeId'
 const FORM_CONF = 'formConf'
 
 export function getDrawingList() {
+  // 加入缓存版本的概念，保证缓存数据与程序匹配
+  const version = localStorage.getItem(DRAWING_ITEMS_VERSION_KEY)
+  if (version !== DRAWING_ITEMS_VERSION) {
+    localStorage.setItem(DRAWING_ITEMS_VERSION_KEY, DRAWING_ITEMS_VERSION)
+    saveDrawingList([])
+    return null
+  }
+
   const str = localStorage.getItem(DRAWING_ITEMS)
   if (str) return JSON.parse(str)
   return null
