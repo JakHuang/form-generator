79b64821c0ba63ecb305db11de1f962537f3d671
JakHuang <352407406@qq.com>
2020-05-04 09:00:04 +0800

    去除 undo

--- src/components/tinymce/config.js
+++ src/components/tinymce/config.js
@@ -4,5 +4,5 @@ export const plugins = [
   'advlist anchor autolink autosave code codesample directionality emoticons fullscreen hr image imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace spellchecker tabfocus table template textpattern visualblocks visualchars wordcount'
 ]
 export const toolbar = [
-  'code searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent blockquote undo redo removeformat subscript superscript codesample hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen'
+  'code searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent blockquote removeformat subscript superscript codesample hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen'
 ]
