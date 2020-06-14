fdb6e5b3a559d9614c6069eb587ba549d740fd89
JakHuang <352407406@qq.com>
2020-06-15 00:09:36 +0800

    no-multiple-empty-lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,8 @@ module.exports = {
     'no-multi-assign': 0,
     'prefer-rest-params': 0,
     'vue/require-prop-types': 0,
-    'no-restricted-globals': 0
+    'no-restricted-globals': 0,
+    'no-multiple-empty-lines': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
