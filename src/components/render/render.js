81b4c7a4b4e4ed63702ddb42e65a5d6a81ecf49f
JakHuang <352407406@qq.com>
2020-07-12 14:44:12 +0800

    feat：json表单中支持——__methods__方法

--- src/components/render/render.js
+++ src/components/render/render.js
@@ -1,13 +1,5 @@
 import { deepClone } from '@/utils/index'
 
-function vModel(self, dataObject, defaultValue) {
-  dataObject.props.value = defaultValue
-
-  dataObject.on.input = val => {
-    self.$emit('input', val)
-  }
-}
-
 const componentChild = {}
 /**
  * 将./slots中的文件挂载到对象componentChild上
@@ -22,46 +14,91 @@ keys.forEach(key => {
   componentChild[tag] = value
 })
 
-export default {
-  render(h) {
-    const dataObject = {
-      attrs: {},
-      props: {},
-      nativeOn: {},
-      on: {},
-      style: {}
+function vModel(dataObject, defaultValue) {
+  dataObject.props.value = defaultValue
+
+  dataObject.on.input = val => {
+    this.$emit('input', val)
+  }
+}
+
+function mountSlotFlies(h, confClone, children) {
+  const childObjs = componentChild[confClone.__config__.tag]
+  if (childObjs) {
+    Object.keys(childObjs).forEach(key => {
+      const childFunc = childObjs[key]
+      if (confClone.__slot__ && confClone.__slot__[key]) {
+        children.push(childFunc(h, confClone, key))
+      }
+    })
+  }
+}
+
+function emitEvents(confClone) {
+  ['on', 'nativeOn'].forEach(attr => {
+    const eventKeyList = Object.keys(confClone[attr] || {})
+    eventKeyList.forEach(key => {
+      const val = confClone[attr][key]
+      if (typeof val === 'string') {
+        confClone[attr][key] = event => this.$emit(val, event)
+      }
+    })
+  })
+}
+
+function buildDataObject(confClone, dataObject) {
+  Object.keys(confClone).forEach(key => {
+    const val = confClone[key]
+    if (key === '__vModel__') {
+      vModel.call(this, dataObject, confClone.__config__.defaultValue)
+    } else if (dataObject[key]) {
+      dataObject[key] = { ...dataObject[key], ...val }
+    } else {
+      dataObject.attrs[key] = val
     }
+  })
+
+  // 清理属性
+  clearAttrs(dataObject)
+}
+
+function clearAttrs(dataObject) {
+  delete dataObject.attrs.__config__
+  delete dataObject.attrs.__slot__
+  delete dataObject.attrs.__methods__
+}
+
+function makeDataObject() {
+  return {
+    attrs: {},
+    props: {},
+    nativeOn: {},
+    on: {},
+    style: {}
+  }
+}
+
+export default {
+  props: {
+    conf: {
+      type: Object,
+      required: true
+    }
+  },
+  render(h) {
+    const dataObject = makeDataObject()
     const confClone = deepClone(this.conf)
     const children = []
 
     // 如果slots文件夹存在与当前tag同名的文件，则执行文件中的代码
-    const childObjs = componentChild[confClone.__config__.tag]
-    if (childObjs) {
-      Object.keys(childObjs).forEach(key => {
-        const childFunc = childObjs[key]
-        if (confClone.__slot__ && confClone.__slot__[key]) {
-          children.push(childFunc(h, confClone, key))
-        }
-      })
-    }
+    mountSlotFlies.call(this, h, confClone, children)
+
+    // 将字符串类型的事件，发送为消息
+    emitEvents.call(this, confClone)
 
     // 将json表单配置转化为vue render可以识别的 “数据对象（dataObject）”
-    Object.keys(confClone).forEach(key => {
-      const val = confClone[key]
-      if (key === '__vModel__') {
-        vModel(this, dataObject, confClone.__config__.defaultValue)
-      } else if (dataObject[key]) {
-        dataObject[key] = { ...dataObject[key], ...val }
-      } else {
-        dataObject.attrs[key] = val
-      }
-    })
-
-    // 清理属性
-    delete dataObject.attrs.__config__
-    delete dataObject.attrs.__slot__
+    buildDataObject.call(this, confClone, dataObject)
 
     return h(this.conf.__config__.tag, dataObject, children)
-  },
-  props: ['conf']
+  }
 }
